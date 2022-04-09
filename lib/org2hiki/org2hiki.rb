3ef75d833458d89d6c233b7bce9cfa380bca9e04
Shigeto R. Nishitani <daddygongon@users.noreply.github.com>
2022-04-09 13:01:15 +0900

    rev for head

--- lib/org2hiki/org2hiki.rb
+++ lib/org2hiki/org2hiki.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
-
-# convert org formated lines to hiki
+# convert org formatted lines to hiki
 class ToHiki
   def initialize()
     @in_example = false
@@ -12,25 +11,41 @@ class ToHiki
     lines.split(/\n/).each do |line|
       next if check_block?(line)
       line = check_in line
-      line = check_one line
+      line = check_head line
       @outputs << line
     end
     @outputs.join("\n")
   end
 
   private
-  def check_one(line)
-    case line_match(line)
-    when 0; convert_head(line)
-    when 1; convert_desc(line)
-    when 2; convert_list(line)
-    else line
+
+  def check_head(line)
+    return line if line == ""
+    m = []
+    if m = line.match(/^(\*+) (.+)$/)
+      return "!" * m[1].size + " #{m[2]}"
+    elsif m = line.match(/^(\s*)\d\. (.+)$/)
+      return "#" * (m[1].size / 3 + 1) + " #{m[2]}"
+    elsif m = line.match(/^- (.+) :: (.+)$/)
+      return ": #{m[1]} : #{m[2]}"
+    elsif m = line.match((/^(\s*)- (.+)$/))
+      return "*" * (m[1].size / 2 + 1) + " #{m[2]}"
+    else
+      return line
     end
   end
+
   def check_in(line)
-    m = line.match(/^\[\[file:(.+)\]\]/) # attach should be on line
-    return m ? convert_attach(m, line) : line
+    m = []
+    if m = line.match(/\[\[file:(.+)\]\]/)
+      return m ? convert_attach(m, line) : line
+    elsif m = line.match(/\[\[(.+)\]\[(.+)\]\]/)
+      return m ? convert_link(m, line) : line
+    else
+      return line
+    end
   end
+
   def check_block?(line)
     m = line.match(/^\#\+(.+)$/)
     if m
@@ -46,47 +61,25 @@ class ToHiki
 
   def check_options(m, line)
     case m[1]
-    when 'begin_example'
+    when "begin_example"
       @in_example = true
-      return '<<<'
-    when 'begin_src ruby'
+      return "<<<"
+    when "begin_src ruby"
       @in_example = true
-      return '<<< ruby'
-    when 'end_src', 'end_example'
+      return "<<< ruby"
+    when "end_src", "end_example"
       @in_example = false
-      return '>>>'
+      return ">>>"
     else
-      return '// ' + line
+      return "// " + line
     end
-  end
-
-  HEAD = Regexp.new(/^(\*+) (.+)$/)
-  DESC = Regexp.new(/^- (.+) :: (.+)$/)
-  LIST = Regexp.new(/^(\s*)- (.+)$/)
-
-  def line_match(line)
-    [HEAD, DESC, LIST].each_with_index do |test, i|
-      return i if line.match(test)
-    end
-  end
-
-  def convert_head(line)
-    m = line.match(HEAD)
-    return m ? ("!" * m[1].size + " #{m[2]}") : line
-  end
-
-  def convert_desc(line)
-    m = line.match(DESC)
-    return m ? (": #{m[1]} : #{m[2]}") : line
-  end
-
-  def convert_list(line)
-    m = line.match(LIST)
-    n = (m[1].size+2) / 2
-    return m ? ("*" * n + " #{m[2]}") : line
   end
 
   def convert_attach(m, line)
-    line.gsub!(m[0],"\{\{attach_view(#{File.basename(m[1])})\}\}")
+    line.gsub!(m[0], "\{\{attach_view(#{File.basename(m[1])})\}\}")
+  end
+
+  def convert_link(m, line)
+    line.gsub!(m[0], "[[#{m[2]}|#{m[1]}]]")
   end
 end
--- test/org2hiki_test.rb
+++ test/org2hiki_test.rb
@@ -2,10 +2,56 @@ require "test_helper"
 
 class Org2hikiTest < Minitest::Test
   def test_that_it_has_a_version_number
+    puts "bundle exec rake test TESTOPTS=\"--name=test_desc -v\""
     refute_nil ::Org2hiki::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_title
+    p line = "#+TITLE: test"
+    p ToHiki.new.convert(line)
+  end
+
+  def test_link
+    line = "[[hoge_url][hoge]]"
+    assert_equal("[[hoge|hoge_url]]",
+                 ToHiki.new.convert(line))
+  end
+
+  def test_head
+    4.times do |i|
+      p line = "*" * i + " hoge"
+      assert_equal("!" * i + " hoge",
+                   ToHiki.new.convert(line))
+    end
+  end
+
+  def test_list
+    p ""
+    3.times do |i|
+      p line = " " * 2 * i + "- hoge"
+      p actual = ToHiki.new.convert(line)
+      assert_equal("*" * (i + 1) + " hoge", actual)
+    end
+  end
+
+  def test_desc
+    p ""
+    p line = "- title :: hoge"
+    p actual = ToHiki.new.convert(line)
+    assert_equal(": title : hoge", actual)
+  end
+
+  def test_enumerate
+    p ""
+    3.times do |i|
+      p line = " " * 3 * i + "1. hoge"
+      p actual = ToHiki.new.convert(line)
+      assert_equal("#" * (i + 1) + " hoge", actual)
+    end
+    3.times do |i|
+      p line = " " * 3 * i + "#{i}. hoge"
+      p actual = ToHiki.new.convert(line)
+      assert_equal("#" * (i + 1) + " hoge", actual)
+    end
   end
 end
