c721d32d0357570c5da7a0b04cf44011a00eb7b7
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2022-10-17 12:28:39 +0900

    rev links

--- lib/org2hiki/org2hiki.rb
+++ lib/org2hiki/org2hiki.rb
@@ -10,7 +10,7 @@ class ToHiki
   def convert(lines)
     lines.split(/\n/).each do |line|
       next if check_block?(line)
-      line = check_in line
+      line = check_link line
       line = check_head line
       @outputs << line
     end
@@ -22,30 +22,29 @@ class ToHiki
   def check_head(line)
     return line if line == ""
     m = []
-    if m = line.match(/^(\*+) (.+)$/)
+    if m = line.match(/^(\*+) (.+)$/) # head
       return "!" * m[1].size + " #{m[2]}"
-    elsif m = line.match(/^(\s*)\d\. (.+)$/)
+    elsif m = line.match(/^(\s*)\d\. (.+)$/) # numbered_list
       return "#" * (m[1].size / 3 + 1) + " #{m[2]}"
-    elsif m = line.match(/^- (.+) :: (.+)$/)
+    elsif m = line.match(/^- (.+) :: (.+)$/) # description
       return ": #{m[1]} : #{m[2]}"
-    elsif m = line.match((/^(\s*)- (.+)$/))
+    elsif m = line.match((/^(\s*)- (.+)$/)) # non-numbered_list
       return "*" * (m[1].size / 2 + 1) + " #{m[2]}"
-    elsif m = line.match((/^\|(.+)\|$/))
+    elsif m = line.match((/^\|(.+)\|$/)) # table
       return convert_table(m, line)
     else
       return line
     end
   end
 
-  def check_in(line)
+  def check_link(line)
     m = []
-    if m = line.match(/\[\[file:(.+)\]\]/)
-      return m ? convert_attach(m, line) : line
-    elsif m = line.match(/\[\[(.+)\]\[(.+)\]\]/)
-      return m ? convert_link(m, line) : line
+    if m = line.match(/\[\[(.+)\]\]/)
+      line2 = recursive_convert_link(line)
     else
-      return line
+      line2 = line
     end
+    return line2
   end
 
   def check_block?(line)
@@ -81,12 +80,50 @@ class ToHiki
     line.gsub!(m[0], "\{\{attach_view(#{File.basename(m[1])})\}\}")
   end
 
-  def convert_link(m, line)
-    line.gsub!(m[0], "[[#{m[2]}|#{m[1]}]]")
+  def recursive_convert_link(line)
+    m0 = line.match(/(\[\[.+?\]\])(.*)/)
+    return if m0 == nil
+    if m0[2].match(/\W/)
+      recursive_convert_link(m0[2]) # for multiple links
+    end
+    m1 = m0[1].match(/\[\[(.+?)\](.*)\]/)
+    return line.gsub!(m1[0], hiki_link(m1))
+  end
+
+  def hiki_link(m1)
+    string = m1[0]
+    string2 = if url?(m1[1]) == nil
+        if m = string.match(/\[\[(.+)\]\[(.+)\]\]/)
+          "{{attach_anchor_string(#{m[2]},#{m[1]})}}"
+        elsif m = string.match(/\[\[(.+)\]\]/)
+          "{{attach_anchor(#{m[1]})}}"
+        end
+      else
+        if m = string.match(/file:(.+)\]\]/) # url? judges file:hoge.pdf == url
+          "{{attach_anchor(#{m[1]})}}"
+        else
+          if m2 = m1[2].match(/\[(.+)\]/)
+            "[[#{m2[1]}|#{m1[1]}]]"
+          else
+            "[[#{m1[1]}]]"
+          end
+        end
+      end
+    return string2
+  end
+
+  require "uri"
+
+  def url?(url_string)
+    URI::DEFAULT_PARSER.make_regexp.match(url_string)
   end
 
   def convert_table(m, line)
     # should be ...
-    line.gsub!("| ", "|| ")[0..-2]
+    if line[0..1] == "|-"
+      nil
+    else
+      line.gsub!("| ", "|| ")[0..-2]
+    end
   end
 end
--- spec/org2hiki_spec.rb
+++ spec/org2hiki_spec.rb
@@ -1,5 +1,5 @@
-require 'spec_helper'
-require 'org2hiki'
+require "spec_helper"
+require "org2hiki"
 
 def assert(org, hiki)
   converted = ToHiki.new.convert(org)
@@ -7,38 +7,47 @@ def assert(org, hiki)
 end
 
 describe ToHiki do
+  it "convert specific_link" do
+    assert("[[https://n.com/ode.ipynb][nb]]", "[[nb|https://n.com/ode.ipynb]]")
+    assert("[[https://n.com/ode.ipynb]]", "[[https://n.com/ode.ipynb]]")
+    assert("[[hoge.pdf][hoge]]", "{{attach_anchor_string(hoge,hoge.pdf)}}")
+    assert("[[hoge.pdf]]", "{{attach_anchor(hoge.pdf)}}")
+    assert("[[file:hoge.pdf]]", "{{attach_anchor(hoge.pdf)}}")
+  end
+
+  it "convert links on line" do
+    assert("[[https://n.com/ode.ipynb][nb]], [[https://n.com/ode.ipynb]]",
+           "[[nb|https://n.com/ode.ipynb]], [[https://n.com/ode.ipynb]]")
+  end
+
   it "convert head lines" do
-    assert('* head1', '! head1')
-    assert('** head1', '!! head1')
-    assert('*** head1', '!!! head1')
+    assert("* head1", "! head1")
+    assert("** head1", "!! head1")
+    assert("*** head1", "!!! head1")
   end
 
   it "convert lists" do
-    assert('- list1', '* list1')
-    assert('  - list2', '** list2')
-    assert('    - list3', '*** list3')
-    assert('      - list4', '**** list4')
+    assert("- list1", "* list1")
+    assert("  - list2", "** list2")
+    assert("    - list3", "*** list3")
+    assert("      - list4", "**** list4")
   end
 
   it "convert description" do
-    assert('- list1 :: hoge', ': list1 : hoge')
-  end
-
-  it "convert attach view" do
-    assert('[[file:./e.gif]]', '{{attach_view(e.gif)}}')
+    assert("- list1 :: hoge", ": list1 : hoge")
   end
 
   it "convert options to comments" do
-    assert('#+hogehoge', '// #+hogehoge')
+    assert("#+hogehoge", "// #+hogehoge")
   end
 
   it "convert example" do
-    org =<<-EOS
+    org = <<-EOS
 #+begin_example
 example
 #+end_example
 EOS
-    hiki =<<-EOS
+    hiki = <<-EOS
 <<<
 example
 >>>
@@ -47,12 +56,12 @@ EOS
   end
 
   it "convert ruby src" do
-    org =<<-EOS
+    org = <<-EOS
 #+begin_src ruby
 ruby
 #+end_src
 EOS
-    hiki =<<-EOS
+    hiki = <<-EOS
 <<< ruby
 ruby
 >>>
